bb2afe4a69f9d38bbac462245b4b6aaf6580e793
zz2901 <zz2901@cumc.columbia.edu>
2024-12-02 18:49:53 -0500

    update clusterbreak pipeline functions

--- R/clusterbreak.R
+++ R/clusterbreak.R
@@ -1,3 +1,45 @@
+#' Modified Louvain Clustering function
+#'
+#' @param gin Input network.
+#' @param res Louvain algorithm sensitivity.
+#' @param res_range Percent of sensitivity for the function loop over for best modularity.
+#' @param res_step Number of even breaks over the sensitivity range.
+#' @param itr Number of iteration over each break point. 
+#' @return A clustered graph network optimized for modularity
+#' @importFrom igraph graph_from_adjacency_matrix E cluster_louvain V layout_with_fr
+#' @export
+louvain_mod<-function(gin,res,res_range_perc=0,res_step=0,itr=3){
+  
+  res=seq(res - res_range_perc*res, res + res_range_perc*res, by = res_step)# set range of resolution parameters
+  
+  best_modularity <- NULL
+  best_clusters <- NULL
+  best_resolution<-NULL
+  for (j in seq_along(res)){
+    if (j==1){
+      best_resolution<-res[j]
+    }
+    for (i in 1:itr) {#iteration loop
+      gin.cluster<-igraph::cluster_louvain(gin,weights=igraph::E(gin)$weight,resolution=res[j]) # cluster using weights
+      gin.mod<-  igraph::modularity(gin.cluster)#obtain modularity
+      
+      if(i==1){#initial loop
+        best_modularity <- gin.mod
+        best_clusters <- gin.cluster
+      }
+      if (i>1 & gin.mod > best_modularity) {#replace initial values if higher modularity
+        best_modularity <- gin.mod
+        best_clusters <- gin.cluster
+        best_resolution<-res[j]
+      }
+    }
+  }
+  
+  return(list(cluster=best_clusters,
+              resolution=best_resolution,
+              modularity=best_modularity))
+}
+
 #' Generate cluster id using graph network and louvain method
 #' 
 #' @param pepmat A square or upper triangular similarity matrix
@@ -7,14 +49,14 @@
 #' @importFrom igraph graph_from_adjacency_matrix E cluster_louvain
 #' @return A vector containing cluster assignment
 #' @export
-
-netcluster<-function(pepmat,igraph_mode="upper",igraph_weight=TRUE,louvain_resolution=1.05){
+netcluster<-function(pepmat,igraph_mode="upper",igraph_weight=TRUE,louvain_resolution=1.05,louvain_range_perc=0,louvain_step=0,louvain_itr=3){
   if(nrow(pepmat)!=ncol(pepmat)) {
     stop("Input must be a square pairwise similarity matrix")
   }
   network<-igraph::graph_from_adjacency_matrix(pepmat,mode=igraph_mode,weighted=igraph_weight)
-  cluster<-igraph::cluster_louvain(network,weights=igraph::E(network)$weight,resolution=louvain_resolution)
-  return(cluster$membership)
+  # cluster.n<-igraph::cluster_louvain(network,weights=igraph::E(network)$weight,resolution=louvain_resolution)
+  cluster.n<-louvain_mod(network,res=louvain_resolution,res_range_perc=louvain_range_perc,res_step=louvain_step,itr=louvain_itr)
+  return(cluster.n$cluster$membership)
 }
 
 #' Generate clusters with specified sizes using graph network and louvain method
@@ -31,7 +73,7 @@ netcluster<-function(pepmat,igraph_mode="upper",igraph_weight=TRUE,louvain_resol
 #' @return A nx2 matrix with a column containing n peptide sequences and their corresponding cluster assignment
 #' @export
 #' 
-clusterbreak <- function(pep, thresh=0.8,k_size=2, hash_size=50, size_max=10, size_min=3, sens=1.05, max_itr=500) {
+clusterbreak <- function(pep, thresh=0.8,k_size=2, hash_size=50, size_max=10, size_min=3, sens=1.05, max_itr=10000) {
   if (size_max <= size_min) {
     stop("size_max must be greater than size_min")
   }
@@ -116,4 +158,48 @@ clusterbreak <- function(pep, thresh=0.8,k_size=2, hash_size=50, size_max=10, si
   rm(state) # reset state global 
   
   return(result)
+}
+
+#' Generate consensus sequence
+#'
+#' @param df Input clusterbreak output df
+#' @return df with first column being unique cluster id and second column being the corresponding consensus sequence
+#' @importFrom Biostrings AAStringSet
+#' @importFrom DECIPHER AlignSeqs ConsensusSequence
+#' @export
+clusterconsensus<-function(df){
+  cluster.id<-unique(df[,2])
+  out.df<-matrix(nrow=0,ncol=2)
+  for (i in 1:length(cluster.id)){
+    df.sub<-df[which(df[,2] == cluster.id[i]),1]
+    aa.set<-Biostrings::AAStringSet(df.sub)
+    aa.set.align<-DECIPHER::AlignSeqs(aa.set)
+    con.seq<-as.character(DECIPHER::ConsensusSequence(aa.set.align))
+    out.df<-rbind(out.df,c(cluster.id[i],con.seq))
+  }
+  return(out.df)
+}
+
+#' Plot consensus sequences for each cluster in a clustered network
+#'
+#' @param df Input clusterconsensus function output
+#' @param k_size minhash kmer size
+#' @param threshold binary threshold for adjacency matrix
+#' @param sens Louvain algorithm sensitivity
+#' @return hash_size hash function size
+#' @importFrom igraph graph_from_adjacency_matrix E cluster_louvain V layout_with_fr
+#' @export
+consensusplot<-function(df,k_size=2, hash_size=50,threshold=0.8,sens=1.05,...){
+  #similarity matrix and adjacency matrix
+  df.hash<-minhash(df[,2],k_size,hash_size)
+  df.hash<-df.hash$dist_matrix
+  df.hash[df.hash<threshold]<-0
+  #plot call
+  g<-igraph::graph_from_adjacency_matrix(df.hash,mode="upper",weighted=TRUE) # base plot
+  g.weight<-igraph::E(g)$weight # edge weight
+  g.cluster<-igraph::cluster_louvain(g,weights=g.weight,resolution=sens) # cluster using weights
+  
+  g.layout<-igraph::layout_with_fr(g, weights = g.weight) #set layout for plot
+  igraph::V(g)$name <- df[,1]#set node names to cluster name
+  g.out<-plot(g.cluster,g,layout = g.layout, ...)
 }
